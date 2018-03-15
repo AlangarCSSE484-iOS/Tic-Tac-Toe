168f9e5c187f5399de4f179a3d16e319eb2acf1b
Vibha <alangavr@rose-hulman.edu>
2018-03-15 12:20:48 -0400

    added nav bar and toolbar - new game button

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pressedNewGame(_ sender: Any) {
+        print("You pressed new game!")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
